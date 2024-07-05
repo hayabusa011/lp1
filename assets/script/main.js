f2114cc302daebad3b263c6f4a8c9d807c534dd9
hayabusa011 <kotukotuweb01@gmail.com>
2024-07-05 19:23:12 +0900

    headerのbuttonをばってんにした

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,10 @@
 // header フェードイン
 const btn = $('.header__hamburger-menu');
 const nav = $('#header-nav');
+const change_btn = $('.hamburger-btn');
 
 btn.on('click', () => {
     nav.toggleClass('open-menu');
+    change_btn.toggleClass('change-btn');
 });
 
